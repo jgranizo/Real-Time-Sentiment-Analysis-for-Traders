ffc75b60982c7616beec063c40fe706cbc4794e3
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-03-24 13:50:25 -0400

    Added Scripts
    Scripts for using reddit api to retrieve essential reddit data

---
+++ createtables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE stocks(
+    OpenPrice VARCHAR(128),
+    ClosePrice VARCHAR(128),
+    Low VARCHAR(128),
+    Volume VARCHAR(128),
+    Ticker VARCHAR(128),
+    Company VARCHAR(128)
+)
