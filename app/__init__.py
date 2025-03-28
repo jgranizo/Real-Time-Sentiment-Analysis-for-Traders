b28f159b830b5288f716a6d3460eec24ee888ccd
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-03-28 19:45:14 -0400

    Added CORS middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import main_bp
 from .reddit_routes import reddit_bp
 from .stock_routes import stock_bp
 from .correlation_routes import correlation_bp
 def create_app():
     app=Flask(__name__)
+    CORS(app)
     app.register_blueprint(main_bp) #route is registered
     app.register_blueprint(reddit_bp,url_prefix='/api/reddit')
     app.register_blueprint(stock_bp,url_prefix='/api/stock')
