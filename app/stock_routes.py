25006f07c926edcc7ae06d5939c59fccc208429e
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 18:55:41 -0400

    created functions to call queries from route parameters
    function checks if start and end date parameter exist,
    function returns all data from given ticker if it exists and date input is not given/exists

--- app/stock_routes.py
+++ app/stock_routes.py
@@ -1,10 +1,22 @@
-from flask import Blueprint,jsonify
-from app.services.stock_service import get_stock_metrics
+from flask import Blueprint,jsonify,request
+from app.services.stock_service import get_stock_data,get_stock_by_date
+
 #create blueprint for stock first
 stock_bp = Blueprint("stock",__name__,)
 #create route for stock
 @stock_bp.route('/<ticker>',methods=['GET'])
-#create function for stock to retrieve dat
-def  get_stock_data(ticker):
-    data = get_stock_metrics(ticker)
-    return jsonify(data)
+#create function for stock to retrieve data
+def  fetch_stock_data(ticker):
+    
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    if start_date and end_date:
+        data = get_stock_by_date(ticker,start_date,end_date)
+        if data:
+            return jsonify(data),200
+        return jsonify({"message": " No data found for the specific date range"}), 404
+   
+    data = get_stock_data(ticker)
+    if data:
+        return jsonify(data), 200
+    return jsonify({"message": "No data found for the specific ticker"}), 404
