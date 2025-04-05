8a513e473ae8b0ac2b2780560ec6b3982cc2c8a7
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 18:56:53 -0400

    Created query functions
    one function queries for all data in with given ticker,
    the other function queries for all data with in  a given range

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -1,3 +1,34 @@
+from app.models import StockData
+from datetime import datetime
+
+def get_stock_data(ticker):
+    try:
+        stocks=StockData.query.filter(StockData.ticker == ticker).all()
+        return [item.to_dict() for item in stocks]
+    except Exception as e:
+        print(f"Error retrieving stock data: {e}")
+        return None
+        
+
+    
+def get_stock_by_date(ticker,start_date=None,end_date=None):
+    try:
+        query = StockData.query.filter(StockData.ticker == ticker)
+        if start_date and end_date:
+            start = datetime.strptime(start_date,"%Y-%m-%d").date()
+            end = datetime.strptime(end_date, "%Y-%m-%d").date()
+            print(start)
+            print(end)
+            query = query.filter(StockData.date>=start).filter(StockData.date<=end)
+        
+        #Query to get a specific recorder by ticker and date
+        result = query.all()
+        return [item.to_dict() for item in result]
+    except Exception as e:
+        print(f"Error retrieving stock data: {e}")
+        return None
+    
+
 def get_stock_metrics(ticker):
     #simulate historical stock prices over 7 days
     sample_prices = [152.1, 150.3,149.8,151.2,153.0,150.9,152.7]
