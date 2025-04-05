0868ea50ed0199d7658f986e46b473ff20cde7bc
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 18:52:54 -0400

    Update __init__.py
    Added postgres credentials, initialized db within the app context

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,39 @@ from .routes import main_bp
 from .reddit_routes import reddit_bp
 from .stock_routes import stock_bp
 from .correlation_routes import correlation_bp
+import os
+from dotenv import load_dotenv
+from .extensions import db
+
+
+load_dotenv()
+
+
 def create_app():
-    app=Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://sentimentapi.jeremygranizo.tech"]}})
-    app.register_blueprint(main_bp) #route is registered
-    app.register_blueprint(reddit_bp,url_prefix='/api/reddit')
-    app.register_blueprint(stock_bp,url_prefix='/api/stock')
-    app.register_blueprint(correlation_bp,url_prefix='/api/correlation')
-    #Routes will be registered here
+    app = Flask(__name__)
+   
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    CORS(
+        app,
+        resources={
+            r"/*": {
+                "origins": [
+                    "http://localhost:5173",
+                    "https://sentimentapi.jeremygranizo.tech",
+                ]
+            }
+        },
+    )
+    
+    
+    app.register_blueprint(main_bp)  # route is registered
+    app.register_blueprint(reddit_bp, url_prefix="/api/reddit")
+    app.register_blueprint(stock_bp, url_prefix="/api/stock")
+    app.register_blueprint(correlation_bp, url_prefix="/api/correlation")
+
+    #Initialize database within app context
+    with app.app_context():
+        db.create_all()
     return app
