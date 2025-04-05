fbff52d740eb2d90e8c90972f442881cf48cd725
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 19:01:06 -0400

    Create extensions.py
    created sqlalchemy instance

---
+++ app/extensions.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+db = SQLAlchemy(model_class=Base)
