1d0e6cfd98a1ed0ebc7277aa717b5eb5a9d99f78
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 20:08:52 -0400

    added reddit post model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, BigInteger,Date,Numeric
+from sqlalchemy import Integer, String, BigInteger,Date,Numeric,Float,DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from .extensions import db
 class StockData(db.Model):
@@ -24,3 +24,37 @@ class StockData(db.Model):
 
     def __repr__(self):
         return f"<StockData(date={self.date}, ticker={self.ticker})>"
+    
+class RedditPostData(db.Model):
+    __tablename__ = 'reddit_posts'
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+     # Reddit post ID (unique identifier from Reddit)
+    post_id: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    score: Mapped[int] = mapped_column(Integer, nullable=False)
+    number_comments: Mapped[int] = mapped_column(Integer, nullable=False)
+    date_of_creation: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    subreddit_name: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    number_upvotes: Mapped[int] = mapped_column(Integer, nullable=False)
+    upvote_ratio: Mapped[float] = mapped_column(Float, nullable=False)
+    num_cross_posts: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "title": self.title,
+            "score": self.score,
+            "number_comments": self.number_comments,
+            "date_of_creation": self.date_of_creation.strftime('%Y-%m-%d %H:%M:%S'),
+            "subreddit_name": self.subreddit_name,
+            "url": self.url,
+            "number_upvotes": self.number_upvotes,
+            "upvote_ratio": self.upvote_ratio,
+            "num_cross_posts": self.num_cross_posts
+        }
+
+    def __repr__(self):
+        return f"<RedditPostData(title={self.title}, subreddit={self.subreddit_name})>"
