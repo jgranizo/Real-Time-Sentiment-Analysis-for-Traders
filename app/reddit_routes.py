6656ad616a615812d43bc1d33547b5ac6787ae61
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 20:09:18 -0400

    added function call for reddit data to be inserted

--- app/reddit_routes.py
+++ app/reddit_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify
-from app.services.reddit_service import get_reddit_metrics
+from app.services.reddit_service import fetch_reddit_post
+
 reddit_bp = Blueprint('reddit', __name__)
 
-@reddit_bp.route('/<ticker>', methods=['GET'])
-def get_reddit_data(ticker):
-    data=get_reddit_metrics(ticker)
+@reddit_bp.route('/reddit_fetch', methods=['GET'])
+def get_reddit_data():
+    data=fetch_reddit_post()
     return jsonify(data)
