941062a853b4d6a772f786bc156ce43dcd0855b7
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-05 20:10:36 -0400

    Update reddit_service.py
    added function to fetch reddit data and insert into database

--- app/services/reddit_service.py
+++ app/services/reddit_service.py
@@ -1,4 +1,76 @@
 import math
+import praw
+import csv
+from datetime import datetime, timedelta, timezone
+from app.models import RedditPostData
+from app.extensions import db
+
+# Initialize Reddit API client with your credentials
+reddit = praw.Reddit(
+    client_id="ZPr2EshdHCAQUHQ1B4LRdQ",
+    client_secret="D_pSNQdQpLFu0xkme74Sket1zGUorg",
+    user_agent="BrandDataCollector/1.0 by u/jgran",
+)
+
+
+# Define criteria
+brand_name = ["Tesla", "Microsft", "Amazon", "Apple"]
+min_score = 1  # Minimum score to filter high-upvote posts
+min_comments = 1  # Minimum number of comments for high engagement
+days_limit = 1000  # Only get posts from the last 7 days
+
+# Calculate the Unix timestamp for the date limit
+start_date = datetime.now(timezone.utc) - timedelta(days=days_limit)
+timestamp_limit_start = start_date.timestamp()
+end_date = datetime.now(timezone.utc)
+timestamp_limit_end = end_date.timestamp()
+# Search for posts mentioning the brand in relevant subreddits
+subreddits = ["technology", "gadgets", "stocks"]
+posts_data = []
+subreddit_dictionary = {}
+
+
+def fetch_reddit_post():
+    for subreddit_name in subreddits:
+        print(f"\nFetching posts about `{brand_name}` in r/{subreddit_name}")
+        subreddit = reddit.subreddit(subreddit_name)
+
+        # Filter posts based on criteria
+
+        for post in subreddit.search(brand_name):
+            if (
+                post.created_utc >= timestamp_limit_start
+                and post.created_utc <= timestamp_limit_end
+            ):
+                existing_post = RedditPostData.query.filter_by(post_id=post.id).first()
+                if existing_post:
+                    print(f"Duplicate post detected: {post.title} - Skipping")
+                    continue
+                post_info = {
+                    "title": post.title,
+                    "score": post.score,
+                    "number_comments": post.num_comments,
+                    "date_of_creation": datetime.fromtimestamp(
+                        post.created_utc
+                    ).strftime("%Y-%m-%d %H:%M:%S"),
+                    "subreddit_name": post.subreddit.display_name,
+                    "url": post.url,
+                    "number_upvotes": post.ups,
+                    "upvote_ratio": post.upvote_ratio,
+                    "num_cross_posts": post.num_crossposts,
+                    "post_id": post.id,
+                }
+                try:
+                    new_post = RedditPostData(**post_info)
+                    db.session.add(new_post)
+                    db.session.commit()
+                    print(f"Stored post: {post.title} from r/{subreddit_name}")
+                except Exception as e:
+                    db.session.rollback()
+                    print(f"Error saving post: {e}")
+
+
+"""
 def get_reddit_metrics(ticker):
     
     sample_posts = [
@@ -43,3 +115,5 @@ def get_reddit_metrics(ticker):
         "virality_score": round(avg_virality,3)
 
     }
+
+    """
