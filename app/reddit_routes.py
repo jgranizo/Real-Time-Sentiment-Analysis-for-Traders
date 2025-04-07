3e763361571a66646c88793ac41b50b3ed49ae38
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-06 20:17:31 -0400

    added api request function to retrieve reddit post with a given ticker

--- app/reddit_routes.py
+++ app/reddit_routes.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, jsonify
-from app.services.reddit_service import fetch_reddit_post
+from app.services.reddit_service import fetch_reddit_post, get_reddit_data
 
 reddit_bp = Blueprint('reddit', __name__)
 
 @reddit_bp.route('/reddit_fetch', methods=['GET'])
-def get_reddit_data():
+def fetching_reddit_data():
     data=fetch_reddit_post()
+    return jsonify(data)
+
+@reddit_bp.route('/<ticker>',methods=['GET'])
+def get_reddit_data_by_ticker(ticker):
+    data = get_reddit_data(ticker)
     return jsonify(data)
