03528456385b8aa449dc094e591edac95da0f7cd
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-08 19:46:17 -0400

    Update stock_service.py
    removed hard coded data

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -28,20 +28,3 @@ def get_stock_by_date(ticker,start_date=None,end_date=None):
         print(f"Error retrieving stock data: {e}")
         return None
     
-
-def get_stock_metrics(ticker):
-    #simulate historical stock prices over 7 days
-    sample_prices = [152.1, 150.3,149.8,151.2,153.0,150.9,152.7]
-
-    mean_price = sum(sample_prices)/len(sample_prices)
-
-    variance = sum((p-mean_price) ** 2 for p in sample_prices)/len(sample_prices)
-    volatility = variance ** 0.5
-
-
-    return{
-        "ticker": ticker,
-        "average_price": round(mean_price,2),
-        "volatility":round(volatility,3),
-        "price_series":sample_prices
-    }
