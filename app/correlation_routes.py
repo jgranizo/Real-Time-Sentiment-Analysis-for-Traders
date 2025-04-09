e40dd1cc876618d3fcbb92cb6869250a6952ac6a
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-09 18:36:13 -0400

    Update correlation_routes.py
    added date parameters

--- app/correlation_routes.py
+++ app/correlation_routes.py
@@ -1,9 +1,16 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint,jsonify,request
 from app.services.correlation_service import get_correlation_metrics
-
+import pandas as pd
 correlation_bp = Blueprint('correlation',__name__)
 #create route for correlation_bp
 @correlation_bp.route('/<ticker>',methods=['GET'])
 def get_correlation_data(ticker):
-    data=get_correlation_metrics(ticker)
-    return jsonify(data)
+
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    if start_date and end_date:
+        data=get_correlation_metrics(ticker,start_date,end_date)
+        if data: 
+            return data,200
+    return jsonify({"message": "No data found for the specified date range"})
+    
