b61d5692c9a720fe29e1a35c4f9fef62fcfcc8dd
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-09 18:41:00 -0400

    Update CorrelationPage.jsx
    created input values for correlation values to call api requests

--- frontend/src/pages/CorrelationPage.jsx
+++ frontend/src/pages/CorrelationPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import { getCorrelationData } from '../services/api'
+import DatePicker from "react-datepicker";
 
-export default function CorrelationPage(){
+import "react-datepicker/dist/react-datepicker.css";
+function CorrelationPage(){
+    const {ticker} = useParams();
+    const [correlationData, setCorrelationData] = useState(null);
+    const [startDate,setStartDate] = useState(null)
+    const [endDate,setEndDate] = useState(null)
+    useEffect(()=>{
+        async function fetchData(){
+            const data = await getCorrelationData(ticker,startDate,endDate);
+            setCorrelationData(data)
+            console.log('recieved!',Object.values(data))
+        }
+        fetchData();
+    },[ticker])
     return(
         <div>
             <p>Correlation Page</p>
+            <p>Reddit Data for {ticker}</p>
+              <div> 
+                <div>
+
+                <DatePicker selected={startDate} on onChange={(startDate)=> setStartDate(startDate)}/>
+                </div>
+                <DatePicker selected={endDate} on onChange={(endDate)=> setEndDate(endDate)}/>
+                    </div>
+            {console.log("Test",correlationData)}
+            {console.log(startDate)}
+            {console.log(endDate)}
+            {correlationData?(<div><p>Data Successfully Aquired!</p>
+            <p>{Array.from(correlationData)}</p>
+            
+            
+            </div>):(<p>Loading data ...</p>)}
         </div>
     )
 }
+
+export default CorrelationPage;
