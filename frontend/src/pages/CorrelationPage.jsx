91fae9314c2ad0d51cdd3afb24376743b22d8dfa
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-04-10 23:33:28 -0400

    Created visualization of data
    created 3 charts to display the correlation between each other, mainly between the stocks prices and the other data

--- frontend/src/pages/CorrelationPage.jsx
+++ frontend/src/pages/CorrelationPage.jsx
@@ -1,57 +1,352 @@
 import React, { useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
+import { data, useParams } from 'react-router-dom'
 import { getCorrelationData } from '../services/api'
 import DatePicker from "react-datepicker";
-
+import * as d3 from 'd3'
 import "react-datepicker/dist/react-datepicker.css";
-function CorrelationPage(){
-    const today = new Date()
-    const correctToday = new Intl.DateTimeFormat('en-CA').format(today);
-    const yesterday = new Date(today)
-    yesterday.setDate(today.getDate()-1)
-    const correctYesterday = new Intl.DateTimeFormat('en-CA').format(yesterday);
-    const {ticker} = useParams();
-    const [correlationData, setCorrelationData] = useState(null);
-    const [startDate,setStartDate] = useState(correctYesterday)
-    const [endDate,setEndDate] = useState(correctToday)
-    useEffect(()=>{
-        async function fetchData(){
-            const data = await getCorrelationData(ticker,startDate,endDate);
-            setCorrelationData(data)
-            
-        }
+import "./css/correlation.css"
+function CorrelationPage() {
+  const today = new Date()
+  const correctToday = new Intl.DateTimeFormat('en-CA').format(today);
+  const yesterday = new Date(today)
+  yesterday.setDate(today.getDate() - 1)
+  const correctYesterday = new Intl.DateTimeFormat('en-CA').format(yesterday);
+  const { ticker } = useParams();
+  const [company, setCompany] = useState(null);
+  const [correlationData, setCorrelationData] = useState(null);
+  const [startDate, setStartDate] = useState(correctYesterday)
+  const [endDate, setEndDate] = useState(correctToday)
+
+  var companyList = ["TSLA", "APPL", "MSFT"]
+  // setting the margins and dimensions of the graph
+  var margin = { top: 10, right: 100, bottom: 30, left: 30 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+
+
+  // append the svg object to the body of the page
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getCorrelationData(ticker, startDate, endDate);
+      setCorrelationData(data)
+
+    }
+    const allGroup = ["TSLA", "MSFT", "APPL"];
+
+    d3.select("#RAI_correlation").selectAll("*").remove();
+    d3.select("#stock-chart").selectAll("*").remove();
+    
+d3.select("#RAI_correlation").select("svg").remove();
+d3.select("#stock-chart").select("svg").remove();
+d3.select("#sentiment-chart").select("svg").remove();
+    // Clear and create dropdown
+    d3.select("#selectButton").selectAll("option").remove();
+    d3.select("#selectButton")
+      .selectAll("myOptions")
+      .data(allGroup)
+      .enter()
+      .append("option")
+      .text(d => d)
+      .attr("value", d => d);
+
+    fetchData();
+  }, [startDate, endDate, company])
+
+
+  useEffect(() => {
+
+    if (!correlationData || typeof correlationData['0'] === 'undefined') {
+      return;
+    }
+
+    // Clear old chart
+    d3.select("#RAI_correlation").selectAll("*").remove();
+    d3.select("#stock-chart").selectAll("*").remove();
+    d3.select("#sentiment-chart").selectAll("*").remove();
+    const data = correlationData['0'][5][0]
+    const dateData = correlationData['0'][5][0]['date']
+    const RAIData = Object.values(correlationData['0'][5][0]['RedditActivityIndex']).flat()
+    const openData = Object.values(correlationData['0'][5][0]['open'])
+    const sentimentData = Object.values(data.Sentiment_Score).map(d => +parseFloat(d));
+    const allDates = Object.values(dateData).flat();
+    const indices = Object.keys(data.date)
+    const dataArray = indices.map(i => {
+      const entry = {};
+      for (const key in data) {
+        entry[key] = data[key][i];
+      }
+      return entry
+    })
+
+    console.log(dataArray)
+    console.log(sentimentData)
+
+
+
+
+    const margin = { top: 10, right: 100, bottom: 30, left: 30 },
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    //create sentiment correlation svg
+    const svg = d3.select("#RAI_correlation")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+      svg.append("text")
+      .attr("class","title")
+      .attr("x", width/2)
+      .attr("y",margin.top)
+      .attr("font-family","sans-serif")
+      .attr("fill","black")
+      .attr("text-anchor","middle")
+      .text("RAI")
+
+    //create stock svg
+    const stock_svg = d3.select("#stock-chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ',' + margin.top + ")")
+
+      stock_svg.append("text")
+      .attr("class","title")
+      .attr("x", width/2)
+      .attr("y",margin.top)
+      .attr("font-family","sans-serif")
+      .attr("fill","black")
+      .attr("text-anchor","middle")
+      .text("Stock")
+
+
+    const sentiment_svg = d3.select("#sentiment-chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + ',' + margin.top + ")")
+
+      sentiment_svg.append("text")
+      .attr("class","title")
+      .attr("x", width/2)
+      .attr("y",margin.top)
+      .attr("font-family","sans-serif")
+      .attr("fill","black")
+      .attr("text-anchor","middle")
+      .text("Sentiment")
+
+
+    // Load CSV and build chart
+
+    const allGroup = ["TSLA", "MSFT", "APPL"];
+
+
+
+    const myColor = d3.scaleOrdinal()
+      .domain(allGroup)
+      .range(d3.schemeSet2);
+
+    const x = d3.scaleTime()
+      .domain(d3.extent(allDates, d => new Date(d)))
+      .range([0, width]);
+
+    //create x axis for sentiment correlation data
+    svg.selectAll(".x-axis")
+      .data([null])
+      .join(
+        enter => enter.append("g")
+          .attr("class", "x-axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x)),
+        update => update
+          .transition()
+          .duration(1000)
+          .call(d3.axisBottom(x))
+      );
+
+    //create x axis for stock data
+    stock_svg.selectAll(".x-axis")
+      .data([null])
+      .join(
+        enter => enter.append("g")
+          .attr("class", "x-axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x)),
+        update => update
+          .transition()
+          .duration(1000)
+          .call(d3.axisBottom(x))
+      );
+
+    sentiment_svg.selectAll(".x-axis")
+      .data([null])
+      .join(
+        enter => enter.append("g")
+          .attr("class", "x-axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x)),
+        update => update
+          .transition()
+          .duration(1000)
+          .call(d3.axisBottom(x))
+      );
+
+
+    const y = d3.scaleLinear()
+      .domain([d3.min(RAIData), d3.max(RAIData)])
+      .range([height, 0]);
+    svg.append("g").call(d3.axisRight(y));
+
+
+    const y_stock = d3.scaleLinear()
+      .domain([d3.min(openData), d3.max(openData)])
+      .range([height, 0]);
+    stock_svg.append("g").call(d3.axisRight(y_stock));
+
+    const y_sentiment = d3.scaleLinear()
+      .domain([d3.min(sentimentData), d3.max(sentimentData)])
+      .range([height, 0]);
+
+
+    sentiment_svg.append("g").call(d3.axisRight(y_sentiment));
+
+    const scatterPlot = svg.append('g')
+      .selectAll("circle")
+      .data(dataArray)
+      .enter()
+      .append("circle")
+      .attr("cx", d => x(new Date(d.date)))
+      .attr("cy", d => y(+parseInt(d.RedditActivityIndex)))
+      .attr("r", 4)
+      .attr("fill", "blue")
+
+    const stockScatterPlot = stock_svg.append("g")
+      .selectAll("circle")
+      .data(dataArray)
+      .enter()
+      .append("circle")
+      .attr("cx", d => x(new Date(d.date)))
+      .attr("cy", d => y_stock(+parseInt(d.open)))
+      .attr("r", 4)
+      .attr("fill", "blue")
+
+
+    const sentimentScatterPlot = sentiment_svg.append("g")
+      .selectAll("circle")
+      .data(dataArray)
+      .enter()
+      .append("circle")
+      .attr("cx", d => x(new Date(d.date)))
+      .attr("cy", d => y_sentiment(+parseFloat(d.Sentiment_Score)))
+      .attr("r", 4)
+      .attr("fill", "blue")
+
+
+
+
+
+    function update(selectedGroup) {
+      const dataFilter = dataArray.map(d => ({
+        date: d.date,
+        RedditActivityIndex: d.RedditActivityIndex,
+      }));
+
+      scatterPlot.join(
+        enter => enter.append("circle")
+          .attr("cx", d => x(new Date(d.date)))
+          .attr("cy", d => y(+parseFloat(d.RedditActivityIndex)))
+          .attr("r", 0)
+          .attr("fill", "blue")
+          .transition().duration(1000)
+          .attr("r", 4),
+
+        update => update
+          .transition().duration(1000)
+          .attr("cx", d => x(new Date(d.date)))
+          .attr("cy", d => y(+parseFloat(d.RedditActivityIndex)))
+          .attr("r", 4),
+
+        exit => exit
+          .transition().duration(500)
+          .attr("r", 0)
+          .remove()
+      );
+    }
+    d3.select("#selectButton").on("change", function () {
+      const selectedOption = d3.select(this).property("value");
+      update(selectedOption);
+    });
+
+  }, [correlationData, startDate, endDate]); // Only runs once on mount
+
+
+
+
+  function setStart(start) {
+    setStartDate(new Intl.DateTimeFormat('en-CA').format(start))
+
+  }
+  function setEnd(end) {
+    setEndDate(new Intl.DateTimeFormat('en-CA').format(end))
+  }
+
+
+
+  return (
+
+    <div>
+      <link rel="stylesheet" href="correlation.css"></link>
+
+      <p>Correlation Page</p>
+      <p>Reddit Data for {ticker}</p>
+      <div>
+
+
         
-        fetchData();
-    },[endDate])
+          <div className='main'>
+          <div className='main-chart'>
+            <div className='container' id="stock-chart"></div>
+            <select id='selectButton'></select></div>
+            <div className='Dates'>
+          <DatePicker selected={startDate} onChange={(startDate) => setStart(startDate)} />
+        
+        <DatePicker selected={endDate} onChange={(endDate) => setEnd(endDate)} />
+      </div>
+      </div>
+      
 
-        function setStart(start){
-                setStartDate( new Intl.DateTimeFormat('en-CA').format(start))
-              
-    }
-    function setEnd(end){
-        setEndDate( new Intl.DateTimeFormat('en-CA').format(end))
-    }
+      
+          </div>
+          <div className='default-charts'>
+          <div className="container" id="RAI_correlation"></div>
+          <div className='container' id="sentiment-chart"></div>
 
-    return(
-        <div>
-            <p>Correlation Page</p>
-            <p>Reddit Data for {ticker}</p>
-              <div> 
-                <div>
+          </div>
 
-                <DatePicker selected={startDate} onChange={(startDate)=> setStart(startDate)}/>
-                </div>
-                <DatePicker selected={endDate} onChange={(endDate)=> setEnd(endDate)}/>
+       
+        
 
-                    </div>
-           
-            {correlationData?(<div><p>Data Successfully Aquired!</p>
-            <p>{console.log(correlationData)}</p>
-            
-            
-            </div>):(<p>Loading data ...</p>)}
-        </div>
-    )
+      {correlationData ? (<div><p>Data Successfully Aquired!</p>
+        <p>
+
+
+        </p>
+        {typeof correlationData['0'] !== 'undefined' && (
+          console.log(correlationData['0'][5][0]['date'])
+        )
+          //correlationDate['0'][5][0] gets dictionary of all the data values
+        }
+        {console.log(Object.keys(correlationData))}
+
+
+      </div>) : (<p>Loading data ...</p>)}
+    </div>
+  )
 }
 
 export default CorrelationPage;
